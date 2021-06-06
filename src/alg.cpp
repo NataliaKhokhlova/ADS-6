b055a1bd07916bc52507dc8ab6ebc29b8fc2b9a6
NataliaKhokhlova <76100365+NataliaKhokhlova@users.noreply.github.com>
2021-06-06 12:54:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,20 +7,19 @@
 
 BST<std::string> makeTree(const char* filename) {
   BST<std::string>tree;
+  char FromAToa = 'a' - 'A';
+  std::string Word = "";
   std::ifstream file(filename);
   if (!file) {
     std::cout << "File error!" << std::endl;
     return tree;
     }
-  char lowercase = 'a' - 'A';
-  std::string word = "";
   while (!file.eof()) {
     char ch = file.get();
     if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
       if (ch >= 'A' && ch <= 'Z')
-        ch += lowercase;
-      }
-    word += ch;
+        Char += FromAToa;
+      Word += Char;
       } else if (word != "") {
       tree.add(word);
       word = "";
